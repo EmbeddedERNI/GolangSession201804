e291b1a5f5740fd89f322c10a4462c7305a13af0
Victor <metalblueberry@gmail.com>
2018-04-28 15:47:59 +0200

    backup

--- hello/main.go
+++ hello/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Mytest {
+	Field1 string `json:"test1`
+}
+
 func main() {
+
 	fmt.Println("vim-go")
+	
+	var err Error
+	if err != nil {
+		panic("oh no")
+	}
+	
+	fmt.Printf("string(out) = %+v\n", string(out))
+	
+	number := 1
+	fmt.Printf("number = %+v\n", number)
+	log.Printf("number = %+v\n", number)
+
+}
+
+func Doit() {
+	fmt.Println("i'm on it")
+	main()
 }
---
+++ hello/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+// import "testing"
+
+func ExampleDoit() {
+
+	Doit()
+
+	//Output:
+	//i'm on it
+	//vim-go
+
+}
